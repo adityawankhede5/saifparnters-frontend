dcc0f24eeb80eeb31a9196f6e8915d9ec80b7110
adityawankhede5 <adityawankhede555@gmail.com>
2020-03-23 12:05:24 +0530

    Updates

---
+++ precache-manifest.5b5fa85b877a168612b662b2f41f9368.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17287d94e1a4bed262bd5a1aa380544b",
+    "url": "/saifpartners-frontend/index.html"
+  },
+  {
+    "revision": "0e86ab786706bac2ac60",
+    "url": "/saifpartners-frontend/static/css/main.6c5e70fc.chunk.css"
+  },
+  {
+    "revision": "6174aa4f7d10e4cac7c2",
+    "url": "/saifpartners-frontend/static/js/2.3c63cf79.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/saifpartners-frontend/static/js/2.3c63cf79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e86ab786706bac2ac60",
+    "url": "/saifpartners-frontend/static/js/main.b053879a.chunk.js"
+  },
+  {
+    "revision": "5cc2eebf94898888f732",
+    "url": "/saifpartners-frontend/static/js/runtime-main.acaa2864.js"
+  }
+]);
